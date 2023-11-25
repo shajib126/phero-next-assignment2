2d2215a0a7ca21b98720ffaea11ca9f2acb231b5
shajib126 <shajib126@gmail.com>
2023-11-26 01:16:29 +0600

    user router created

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { UserControllers } from './user.controller'
 import { UserServices } from './user.service'
 const router = express()
-
+//
 router.post('/users',UserControllers.createUser).get('/users',UserControllers.getAllUser).get('/users/:userId',UserControllers.getSingleUser).put('/users/:userId',UserControllers.updateUser).delete('/users/:userId',UserControllers.deleteUser).put('/users/:userId/orders',UserControllers.updateOrder).get('/users/:userId/orders',UserControllers.orderByUserId).get('/users/:userId/orders/total-price',UserControllers.calculatePrice)
 
 export const UserRoutes = router
